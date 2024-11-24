a55b8f74fa02758c215a1ea420415671be5a60e2
Gianfranco Palumbo <gianpa@gmail.com>
2024-11-24 22:16:59 +0100

    add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
+import icon from 'astro-icon';
+import compressor from 'astro-compressor';
 
 export default defineConfig({
+  output: 'static',
+  site: 'https://shopifychat.bot',
   integrations: [
     tailwind(),
     icon({
       include: {
-        mdi: ['twitter', 'github', 'linkedin']
-      }
-    })
-  ]
+        mdi: ['twitter', 'github', 'linkedin'],
+      },
+    }),
+    compressor(),
+  ],
+  prefetch: true,
 });
